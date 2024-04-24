d9c2881c8f2f8871e1fa1bf6ca949b8b4666711f
arif qasemi <109466958+arifqasemi@users.noreply.github.com>
2024-04-24 15:20:00 +0700

    Update hero.js

--- app/components/hero.js
+++ app/components/hero.js
@@ -19,7 +19,7 @@ function Hero() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3000/api/posts');
+        const response = await fetch('https://next-js-app-six-theta.vercel.app/api/posts');
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
